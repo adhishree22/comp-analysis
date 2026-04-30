d4894e07f690c4a6acfdf35acf0c4bbf322333d9
Adhishree22 <adhishree2207@gmail.com>
2026-04-30 21:33:18 +0000

    Added

--- src/pipeline.py
+++ src/pipeline.py
@@ -9,31 +9,29 @@ from .price_loader import *
 
 def build_dataset():
 
-  financial_data = build_all_financials(TICKERS)
-  all_data = []
+    all_data = []
 
-  for ticker in TICKERS:
-    
-    income, _, _ = get_financial_statements(ticker)
-    price_df = get_price_data(ticker, income)
+    for ticker in TICKERS:
 
-    df = financial_data[financial_data["Ticker"] == ticker]
+      financial_df = build_all_financials([ticker])
+      income, _, _ = get_financial_statements(ticker)
 
-    df = pd.merge(df, price_df, on=["Ticker", "Year"], how="left")
+      price_df = get_price_data(ticker, income)
 
-    stock = yf.Ticker(ticker)
+      df = pd.merge(financial_df, price_df, on=["Ticker", "Year"], how="left")
 
-    shares = stock.info.get("sharesOutstanding", None)
-    market_cap = stock.info.get("marketCap",None)
+      stock = yf.Ticker(ticker)
+      shares = stock.info.get("sharesOutstanding", None)
+      market_cap = stock.info.get("marketCap",None)
+      df["Shares"] = shares
+      df["MC"] = market_cap
+      df["EV_s"] = stock.info.get("enterpriseValue",None)
+      df["Market_Cap"] = df["Close"] * df["Shares"]
 
-    df["Shares"] = shares
-    df["MC"] = market_cap
-    df["EV_s"] = stock.info.get("enterpriseValue",None)
-    df["Market_Cap"] = df["Close"] * df["Shares"]
+      all_data.append(df)
 
-    all_data.append(df)
+      print(f"{ticker} merged successfully")
 
-    print(f"{ticker} merged successfully")
 
     final_data = pd.concat(all_data, ignore_index=True)
 
