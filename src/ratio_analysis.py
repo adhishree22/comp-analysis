559a5c9e0b2a731cefe198e6528b35a43178a0af
Adhishree22 <adhishree2207@gmail.com>
2026-05-05 22:22:41 +0000

    Added

--- src/ratio_analysis.py
+++ src/ratio_analysis.py
@@ -7,46 +7,46 @@ def safe_divide(a, b):
     return a.div(b.replace(0, float("nan")))
 
 def add_ratios(df):
-
-    df = df.copy()
-
+    
     df = df.sort_values(["Ticker", "Year"]).reset_index(drop=True)
 
-    df.replace([float("inf"), -float("inf")], None, inplace=True)
-
+    ratios = pd.DataFrame()
+    ratios["Ticker"] = df["Ticker"]
+    ratios["Year"]   = df["Year"]
+    
     for col in ["Revenue", "NetIncome", "EBITDA", "EBIT","FCF"]:
-      df[f"{col}_Growth"] = df.groupby("Ticker")[col].pct_change()
+      ratios[f"{col}_Growth"] = df.groupby("Ticker")[col].pct_change()
 
-    df["Net_Margin"] = safe_divide(df["NetIncome"], df["Revenue"])
-    df["Operating_Margin"] = safe_divide(df["EBIT"], df["Revenue"])
-    df["EBITDA_Margin"] = safe_divide(df["EBITDA"], df["Revenue"])
-    df["FCF_Margin"] = safe_divide(df["FCF"], df["Revenue"])
-    df["ROE"] = safe_divide(df["NetIncome"], df["Equity"])
-    df["ROA"] = safe_divide(df["NetIncome"], df["TotalAssets"])
+    ratios["Net_Margin"] = safe_divide(df["NetIncome"], df["Revenue"])
+    ratios["Operating_Margin"] = safe_divide(df["EBIT"], df["Revenue"])
+    ratios["EBITDA_Margin"] = safe_divide(df["EBITDA"], df["Revenue"])
+    ratios["FCF_Margin"] = safe_divide(df["FCF"], df["Revenue"])
+    ratios["ROE"] = safe_divide(df["NetIncome"], df["Equity"])
+    ratios["ROA"] = safe_divide(df["NetIncome"], df["TotalAssets"])
 
-    df["Incremental_Operating_Margin"] = safe_divide(df.groupby("Ticker")["EBIT"].diff() , df.groupby("Ticker")["Revenue"].diff())
-    df["Incremental_EBITDA_Margin"] = safe_divide(df.groupby("Ticker")["EBITDA"].diff() , df.groupby("Ticker")["Revenue"].diff())
-    df["Incremental_Net_Margin"] = safe_divide(df.groupby("Ticker")["NetIncome"].diff() , df.groupby("Ticker")["Revenue"].diff())
-    df["Incremental_FCF_Margin"] = safe_divide(df.groupby("Ticker")["FCF"].diff() , df.groupby("Ticker")["Revenue"].diff())
+    ratios["Incremental_Operating_Margin"] = safe_divide(df.groupby("Ticker")["EBIT"].diff() , df.groupby("Ticker")["Revenue"].diff())
+    ratios["Incremental_EBITDA_Margin"] = safe_divide(df.groupby("Ticker")["EBITDA"].diff() , df.groupby("Ticker")["Revenue"].diff())
+    ratios["Incremental_Net_Margin"] = safe_divide(df.groupby("Ticker")["NetIncome"].diff() , df.groupby("Ticker")["Revenue"].diff())
+    ratios["Incremental_FCF_Margin"] = safe_divide(df.groupby("Ticker")["FCF"].diff() , df.groupby("Ticker")["Revenue"].diff())
 
 
-    df["FCF_Conversion"] = safe_divide(df["FCF"],  df["NetIncome"])
-    df["OCF_to_NetIncome"] = safe_divide(df["OCF"],  df["NetIncome"])
-    df["FCF_to_EBITDA"] = safe_divide(df["FCF"],  df["EBITDA"])
+    ratios["FCF_Conversion"] = safe_divide(df["FCF"],  df["NetIncome"])
+    ratios["OCF_to_NetIncome"] = safe_divide(df["OCF"],  df["NetIncome"])
+    ratios["FCF_to_EBITDA"] = safe_divide(df["FCF"],  df["EBITDA"])
 
-    df["Debt_to_Equity"] = safe_divide(df["TotalDebt"], df["Equity"])
-    df["DebtToEBITDA"] = safe_divide(df["TotalDebt"], df["EBITDA"])
-    df["InterestCoverage"] = safe_divide(df["EBIT"], df["Interest"])
-    df["NetDebtToEBITDA"] = safe_divide(df["NetDebt"],   df["EBITDA"])
+    ratios["Debt_to_Equity"] = safe_divide(df["TotalDebt"], df["Equity"])
+    ratios["DebtToEBITDA"] = safe_divide(df["TotalDebt"], df["EBITDA"])
+    ratios["InterestCoverage"] = safe_divide(df["EBIT"], df["Interest"])
+    ratios["NetDebtToEBITDA"] = safe_divide(df["NetDebt"],   df["EBITDA"])
 
-    df["PE"] = safe_divide(df["MarketCap"], (df["NetIncome"] * Scale))
-    df["EV_EBITDA"] = safe_divide(df["EV"], (df["EBITDA"] * Scale))
-    df["EV_Revenue"]     = safe_divide(df["EV"], (df["Revenue"] * Scale))
-    df["FCF_Yield"] = safe_divide((df["FCF"] * Scale), df["MarketCap"])
+    ratios["PE"] = safe_divide(df["MarketCap"], (df["NetIncome"] * Scale))
+    ratios["EV_EBITDA"] = safe_divide(df["EV"], (df["EBITDA"] * Scale))
+    ratios["EV_Revenue"]     = safe_divide(df["EV"], (df["Revenue"] * Scale))
+    ratios["FCF_Yield"] = safe_divide((df["FCF"] * Scale), df["MarketCap"])
 
-    df["Earnings_Volatility"] = df.groupby("Ticker")["NetIncome_Growth"].transform("std")
+    ratios["Earnings_Volatility"] = df.groupby("Ticker")["NetIncome_Growth"].transform("std")
 
-    df.replace([float("inf"), -float("inf")], float("nan"), inplace=True)
-    df = df.round(4)
+    ratios.replace([float("inf"), -float("inf")], float("nan"), inplace=True)
+    ratios = ratios.round(4)
 
-    return df
+    return ratios
