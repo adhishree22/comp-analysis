704d7abdafa244c4ca88f43b8329e53e838f56b3
Adhishree22 <adhishree2207@gmail.com>
2026-05-06 00:28:47 +0000

    Added

--- data/export_tableau.py
+++ data/export_tableau.py
@@ -7,10 +7,22 @@ def export_for_tableau(data, ratios):
     fin = data.copy()
     rat = ratios.copy()
 
+    for cols in fin.columns
+
     fin["Company"]        = fin["Ticker"].map(Company)
     rat["Company"]        = rat["Ticker"].map(Company)
     fin["Business_Model"] = fin["Ticker"].map(Company_Type)
     rat["Business_Model"] = rat["Ticker"].map(Company_Type)
+    
+    Skip = ["Ticker", "Year", "Company", "Business_Model", "EPS", "Shares"]
+    
+    numeric_cols = [
+        c for c in fin.columns
+        if c not in Skip
+        and pd.api.types.is_numeric_dtype(fin[c])
+        ]
+
+    fin[numeric_cols] = fin[numeric_cols] * Scale
 
     fin = fin.sort_values(["Ticker", "Year"]).reset_index(drop=True)
     rat = rat.sort_values(["Ticker", "Year"]).reset_index(drop=True)
